d6d090fdca5f0f4f1128db0495ac132ff2fc8434
Nick Chabot <nchabot14@gmail.com>
2026-04-15 08:55:53 -0700

    fix: use MYSQL_DB env var per spec

--- pipeline.py
+++ pipeline.py
@@ -21,7 +21,7 @@ def extract():
     conn = pymysql.connect(
         host=os.environ['MYSQL_HOST'],
         port=int(os.environ['MYSQL_PORT']),
-        database=os.environ['MYSQL_DATABASE'],
+        database=os.environ['MYSQL_DB'],
         user=os.environ['MYSQL_USER'],
         password=os.environ['MYSQL_PASSWORD'],
         cursorclass=pymysql.cursors.DictCursor,
